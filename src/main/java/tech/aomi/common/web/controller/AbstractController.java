bbc459ee633bdd48b359a66c513ece3b7ac56e82
Sean <sean.snow@live.com>
2019-12-05 15:48:08 +0800

    描述

--- src/main/java/tech/aomi/common/web/controller/AbstractController.java
+++ src/main/java/tech/aomi/common/web/controller/AbstractController.java
@@ -2,8 +2,6 @@ package tech.aomi.common.web.controller;
 
 import tech.aomi.common.exception.ErrorCode;
 
-import java.io.Serializable;
-
 /**
  * @author 田尘殇Sean(sean.snow @ live.com) createAt 2016/12/26
  */
@@ -15,6 +13,10 @@ public class AbstractController {
 
     protected Result success(Object payload) {
         return Result.create(ErrorCode.SUCCESS, ErrorCode.SUCCESS.getMessage(), payload);
+    }
+
+    protected Result success(Object payload, String describe) {
+        return Result.create(ErrorCode.SUCCESS, describe, payload);
     }
 
 }
