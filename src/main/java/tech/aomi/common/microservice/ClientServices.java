0bab01b749d4ed457aa64cb907a2d0c863fae346
Sean <sean.snow@live.com>
2019-12-27 14:10:37 +0800

    15

--- src/main/java/tech/aomi/common/microservice/ClientServices.java
+++ src/main/java/tech/aomi/common/microservice/ClientServices.java
@@ -9,5 +9,12 @@ public interface ClientServices {
 
     <T> T newInstance(ServiceInstance instance, Class<T> clazz);
 
+    /**
+     * 获取一个随机实例
+     *
+     * @param name 服务名称
+     * @return 服务实例
+     */
+    ServiceInstance getRandomInstance(String name);
 
 }
--- src/main/java/tech/aomi/common/microservice/Retrofit2ClientServices.java
+++ src/main/java/tech/aomi/common/microservice/Retrofit2ClientServices.java
@@ -2,17 +2,27 @@ package tech.aomi.common.microservice;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.util.CollectionUtils;
 import retrofit2.Retrofit;
 import tech.aomi.common.web.client.retrofit2.ClientFactory;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * @author Sean Create At 2019/12/25
  */
 @Slf4j
 public class Retrofit2ClientServices implements ClientServices {
+
+    private final DiscoveryClient discoveryClient;
+
+    public Retrofit2ClientServices(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
+    }
 
     private Map<String, Retrofit> cache = new HashMap<>();
 
@@ -23,6 +33,44 @@ public class Retrofit2ClientServices implements ClientServices {
         String baseUrl = instance.getScheme() + instance.getHost();
         Retrofit retrofit = getRetrofit(baseUrl);
         return retrofit.create(clazz);
+    }
+
+    /**
+     * 模糊匹配服务名字，找到对应的服务
+     *
+     * @param name 服务名称
+     * @return 服务实例
+     */
+    @Override
+    public ServiceInstance getRandomInstance(String name) {
+        List<String> services = discoveryClient.getServices();
+        LOGGER.debug("all services: {}", services);
+        if (CollectionUtils.isEmpty(services))
+            return null;
+
+        for (String serviceId : services) {
+            if (serviceId.toLowerCase().contains(name.toLowerCase())) {
+                List<ServiceInstance> instances = discoveryClient.getInstances(serviceId);
+                if (CollectionUtils.isEmpty(instances)) {
+                    LOGGER.error("服务{}没有运行任何实例", serviceId);
+                    return null;
+                }
+                LOGGER.debug("随机获取一个服务实例");
+                Random random = new Random();
+                int index = random.nextInt(instances.size());
+                ServiceInstance instance = instances.get(index);
+                LOGGER.debug("instanceId: {}, serviceId: {}, host: {}, port: {}, uri: {}, metadata: {}",
+                        instance.getInstanceId(),
+                        instance.getServiceId(),
+                        instance.getHost(),
+                        instance.getPort(),
+                        instance.getUri(),
+                        instance.getMetadata()
+                );
+                return instance;
+            }
+        }
+        return null;
     }
 
     private Retrofit getRetrofit(String baseUrl) {
