338c6245a3c6e3ddec545e79de4ec4ea1b942be7
Sean <sean.snow@live.com>
2020-01-03 15:15:50 +0800

    新增参数

--- src/main/java/tech/aomi/common/web/security/UserDetailsService.java
+++ src/main/java/tech/aomi/common/web/security/UserDetailsService.java
@@ -3,6 +3,8 @@ package tech.aomi.common.web.security;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.Map;
+
 /**
  * 根据用户ID查找用户
  *
@@ -13,12 +15,17 @@ public interface UserDetailsService extends org.springframework.security.core.us
     /**
      * 加载用户信息通过ID
      *
-     * @param id 用户ID
+     * @param id   用户ID
+     * @param args 其他参数
      * @return 用户信息
      * @throws UsernameNotFoundException
      */
-    default UserDetails loadUserById(String id) throws UsernameNotFoundException {
+    default UserDetails loadUserById(String id, Map<String, ?> args) throws UsernameNotFoundException {
         return null;
     }
 
+    @Override
+    default UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
