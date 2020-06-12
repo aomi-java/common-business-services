dabdac95c08a31acf39551a91917ae7d7ebc63df
sean <sean.snow@live.com>
2020-06-12 17:13:40 +0800

    id 不存在自动创建

--- src/main/java/tech/aomi/common/web/MDCTaskDecorator.java
+++ src/main/java/tech/aomi/common/web/MDCTaskDecorator.java
@@ -4,11 +4,17 @@ import org.slf4j.MDC;
 import org.springframework.core.task.TaskDecorator;
 
 import java.util.Map;
+import java.util.UUID;
 
 public class MDCTaskDecorator implements TaskDecorator {
 
+    private static final String ID = "logId";
+
     @Override
     public Runnable decorate(Runnable runnable) {
+        if (null == MDC.get(ID)) {
+            MDC.put(ID, UUID.randomUUID().toString().replaceAll("-", ""));
+        }
         Map<String, String> contextMap = MDC.getCopyOfContextMap();
         return () -> {
             try {
